fa955f7a0b590a0811ff22e75d64d3618e4f7cdf
Slug-Boi <thph@itu.dk>
2024-01-17 19:32:27 +0100

    Added the new and changed config files as well as the desktop bg changing program

--- monitors/2-1920x1080.sh
+++ monitors/2-1920x1080.sh
@@ -2,7 +2,7 @@
 
 # If all displays should be in use, insert this instead of --off
 # --primary --mode 3840x2400 --pos 1924x2160 --rotate normal \
-xrandr --dpi 192 \
+xrandr --dpi 96 \
   --output eDP-1 --off \
   --output DP-2-1 --primary --mode 1920x1080 --scale 2x2 --pos 0x0 --rotate normal \
   --output DP-3 --mode 1920x1080 --scale 2x2 --pos 3840x0 --rotate normal
---
+++ monitors/sluggy-monitors.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xrandr --dpi 96 --fb 7680x4320 \
+    --output DP-2 --mode 3840x2160 \
+    --output DP-4 --scale 2x2 --pos 3840x0 --panning 3840x2160+3840+0

951cb3b5e726f0c2145e38cd58706bcaa50c904f
Slug-Boi <theis.p.holm@gmail.com>
2024-05-11 00:28:10 +0200

    add useOSProber to bootloader (dualboot)

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -5,8 +5,9 @@
         enable = lib.mkForce true;
         efiSupport = true;
         efiInstallAsRemovable = true;
+        useOSProber = true;
         # Will be set to "device" by disko
-        devices = [diskPath];
+        devices = ["nodev"];
       };
     };
   };

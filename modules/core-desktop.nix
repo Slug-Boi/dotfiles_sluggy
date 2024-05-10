13126836a2b3d6482f36a3a44523bafaad70ef80
Slug-Boi <theis.p.holm@gmail.com>
2024-05-11 00:28:45 +0200

    add monitor xorg settings to desktop.nix (this is really bad don't look)

--- modules/core-desktop.nix
+++ modules/core-desktop.nix
@@ -35,5 +35,42 @@
 
   services = {
     dbus.packages = [pkgs.gcr];
+    xserver.monitorSection = ''
+    VendorName     "Unknown"
+    ModelName      "Ancor Communications Inc VG248"
+    HorizSync       30.0 - 160.0
+    VertRefresh     50.0 - 150.0
+    Option         "DPMS"
+    '';
+    xserver.deviceSection = ''
+    Driver         "nvidia"
+    VendorName     "NVIDIA Corporation"
+    BoardName      "NVIDIA GeForce RTX 2070 SUPER"
+    '';
+    xserver.screenSection = ''
+    DefaultDepth    24
+    Option         "Stereo" "0"
+    Option         "nvidiaXineramaInfoOrder" "DFP-0"
+    Option         "metamodes" "DP-0: 1920x1080_144 +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-2: nvidia-auto-select +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+    Option         "SLI" "Off"
+    Option         "MultiGPU" "Off"
+    Option         "BaseMosaic" "off"
+    SubSection     "Display"
+        Depth       24
+    EndSubSection
+    '';
+    # xserver.xrandrHeads = [
+    #  {
+    #    output = "DP-0";
+    #    primary = true;
+    #    monitorConfig = "VertRefresh 144-144";
+    #  }
+    #  {
+    #    monitorConfig = ''
+    #      Option "Enable" "true"
+    #    '';
+    #    output = "DP-2";
+    #  }
+    # ];
   };
 }

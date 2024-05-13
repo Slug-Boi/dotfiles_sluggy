b4ab0fac8c20f0479f9dee4752f44d986be3e91a
Slug-Boi <theis.p.holm@gmail.com>
2024-05-13 15:24:04 +0200

    moved things around

--- home/vscode.nix
+++ home/vscode.nix
@@ -4,13 +4,13 @@
     extensions = with pkgs.vscode-extensions; [
       github.copilot
       vscodevim.vim
+      ms-dotnettools.vscode-dotnet-pack
       ms-toolsai.jupyter
       ms-toolsai.jupyter-keymap
       ms-toolsai.jupyter-renderers
       ms-toolsai.vscode-jupyter-cell-tags
       ms-toolsai.vscode-jupyter-slideshow
-      Ionide.Ionide-fsharp
-      ms-dotnettools.vscode-dotnet-pack
+      #Ionide.Ionide-fsharp
       ms-vsliveshare.vsliveshare
       ms-vscode.cpptools
       golang.Go

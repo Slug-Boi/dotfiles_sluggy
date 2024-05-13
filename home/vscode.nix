73b00e9f058ea20fc6879978a98d1b2082f78341
Slug-Boi <theis.p.holm@gmail.com>
2024-05-13 15:27:54 +0200

    Removed pkgs that were not supported

--- home/vscode.nix
+++ home/vscode.nix
@@ -4,12 +4,12 @@
     extensions = with pkgs.vscode-extensions; [
       github.copilot
       vscodevim.vim
-      ms-dotnettools.vscode-dotnet-pack
       ms-toolsai.jupyter
       ms-toolsai.jupyter-keymap
       ms-toolsai.jupyter-renderers
       ms-toolsai.vscode-jupyter-cell-tags
       ms-toolsai.vscode-jupyter-slideshow
+      #ms-dotnettools.vscode-dotnet-pack
       #Ionide.Ionide-fsharp
       ms-vsliveshare.vsliveshare
       ms-vscode.cpptools

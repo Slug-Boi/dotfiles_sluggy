741eeaebfeaf8dae99a3cd1dcbf87ff103a69e27
Theis <thph@itu.dk>
2024-09-16 13:33:45 +0200

    add cocommit package to home manager

--- home/cocommit/default.nix
+++ home/cocommit/default.nix
@@ -1,38 +1,16 @@
-{ stdenv, lib, buildGoModule, fetchFromGitHub }:
+{
+  pkgs,
+  config,
+  variables,
+  ...
+}: {
+  home.packages = [
+    (pkgs.callPackage ../../pkgs/cocommit/ {})
+  ];
 
-buildGoModule rec {
-
-
-  pname = "cocommit";
-  version = "0.3.0";
-
-  src = fetchFromGitHub {
-    owner = "Slug-Boi";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-oraadDCyek8qnXFZPj2yitXL3KPK+uW1/VZphrEa/MU=";
-  };
-
-  vendorHash = null;
-
-  buildPhase = ''
-    make build-nix
-  '';
-  
-  doCheck = false;
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp "src_code/go_src/${pname}" "$out/bin/${pname}"
-    chmod +x $out/bin/${pname}
-  '';
-
-  makefile = "makefile";
-
-  meta = with lib; {
-    description = "Cocommit is a CLI that makes it easier to co-author users on git commits";
-    homepage = "https://github.com/Slug-Boi/${pname}";
-    license = licenses.mit;
-    platforms = platforms.unix;
-  };
+  # Must be installed manually
+  # Might not be needed as cocommit creates its own author file
+  #xdg.configFile.kitty = {
+    #source = config.lib.file.mkOutOfStoreSymlink "${variables.dotfilesLocation}" + (builtins.toPath "/home/cocommit/");
+ # };
 }

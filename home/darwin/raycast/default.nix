e82f663c451c7d378676d349d7eaeac95e9a5433
Slug-Boi <github.unshipped609@slmail.me>
2024-12-06 20:54:26 +0100

    remove raycast

--- home/darwin/raycast/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  config,
-  variables,
-  ...
-}: {
-  xdg.configFile.raycast = {
-    source = config.lib.file.mkOutOfStoreSymlink "${variables.dotfilesLocation}" + (builtins.toPath "/home/darwin/raycast/config");
-  };
-}

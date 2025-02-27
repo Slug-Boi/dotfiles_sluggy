b9b20bce7808c12376efc138a2ca6ab60d165068
Slug-Boi <github.unshipped609@slmail.me>
2025-02-27 18:30:03 +0100

    add kitty and rename kitty to katti

--- home/katti/default.nix
+++ home/katti/default.nix
@@ -4,13 +4,12 @@
   variables,
   ...
 }: {
-  home.packages = with pkgs; [
-    kitty
+  home.packages = [
+    (pkgs.callPackage ../../pkgs/kitty {})
   ];
-  
+
+  # Must be installed manually
   xdg.configFile.kitty = {
     source = config.lib.file.mkOutOfStoreSymlink "${variables.dotfilesLocation}" + (builtins.toPath "/home/kitty/config");
-    
   };
-
- }
+}

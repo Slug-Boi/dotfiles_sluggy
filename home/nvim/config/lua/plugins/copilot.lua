694a76a0afe96f409423788cf238b64af60e8045
Slug-Boi <theis.p.holm@gmail.com>
2025-09-10 08:50:01 +0200

    disable copilot by default

--- home/nvim/config/lua/plugins/copilot.lua
+++ home/nvim/config/lua/plugins/copilot.lua
@@ -6,6 +6,8 @@ return {
       vim.g.copilot_assume_mapped = true
       vim.g.copilot_tab_fallback = true
 
+      vim.g.copilot_enabled = false
+
       vim.g.copilot_filetypes = {
         markdown = true,
       }
